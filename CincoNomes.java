a8badf29023ac6a299993b952ed7f06a99f76624
MADUBARROSS <mnbarrospereira@gmail.com>
2024-05-15 13:54:44 -0300

    ola

---
+++ CincoNomes.java
@@ -0,0 +1,21 @@
+package vetor;
+
+import java.util.Scanner;
+
+public class CincoNomes {
+
+	public static void main(String[] args) {
+		String nome [] = new String [5];
+		Scanner ler = new Scanner (System.in);
+		for (int i =0; i>5; i++) {
+			System.out.println("informe o nome " + i+"..:");
+			nome [i] = ler.next();	
+		}
+		for (int i =0; i>5; i++) {
+			System.out.println(nome[i]);
+		}
+		ler.close();
+
+	}
+
+}
